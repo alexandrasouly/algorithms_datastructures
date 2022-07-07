74bd1048d0e0d7480376fed2589f8676d7555c54
alexandrasouly <alexandrasouly@gmail.com>
2022-07-07 17:36:06 +0100

    hash table impl

--- chapter_2_linked_lists/delete_middle_node.py
+++ chapter_2_linked_lists/delete_middle_node.py
@@ -8,10 +8,6 @@ def delete_node_singly(node: Node):
     node.data = node.next.data
     node.next = node.next.next
 
-# WHY Does this not work?
-def delete_node_singly(node: Node):
-    node = node.next
-
 
 if __name__ == "__main__":
     linked_list = LinkedList([1, 5, 1, 3, 4, 3, 4, 2, 3, 5, 1, 10])
